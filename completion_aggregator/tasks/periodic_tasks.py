c1ed3608c9caa2d2489cc23c05ef566c7b2df23c
Omar Al-Ithawi <i@omardo.com>
2020-05-23 00:10:49 +0300

    celerybeat: Ensure COMPLETION_AGGREGATOR_ASYNC_AGGREGATION is enabled

--- completion_aggregator/tasks/periodic_tasks.py
+++ completion_aggregator/tasks/periodic_tasks.py
@@ -2,7 +2,11 @@
 Tasks for the CeleryBeat scheduler.
 """
 
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
 from celery.app.base import Celery
+
 
 from ..batch import (
     perform_cleanup,
@@ -12,5 +16,11 @@ from ..batch import (
 
 @Celery.task
 def perform_then_cleanup():
+    if not settings.COMPLETION_AGGREGATOR_ASYNC_AGGREGATION:
+        raise ImproperlyConfigured(
+            'Completion Aggregator configuration error: Both COMPLETION_AGGREGATOR_ASYNC_AGGREGATION is a'
+            'pre-requisite for COMPLETION_AGGREGATOR_ENABLE_CELERY_BEAT to avoid duplicate processing.'
+        )
+
     perform_aggregation()
     perform_cleanup()
